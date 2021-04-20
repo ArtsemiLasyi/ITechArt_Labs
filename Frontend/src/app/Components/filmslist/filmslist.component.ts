14e82651c3f1b61ed709d0f33ebe6bf6421a099a
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2021-04-20 11:27:29 +0300

    Created components "header" (includes navigation), "footer", "filmsli… (#20)
    * Created components "header" (includes navigation), "footer", "filmslist". Added new logo. Created site design
    
    * Removed attributes "style" (all content was moved to .css files). Writed "baseHref" setting
    
    * Substituted relative path in app.module.ts

---
+++ Frontend/src/app/Components/filmslist/filmslist.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filmslist',
+  templateUrl: './filmslist.component.html',
+  styleUrls: ['./filmslist.component.css']
+})
+export class FilmsListComponent { }
--- Frontend/src/app/Modules/app/app.module.ts
+++ Frontend/src/app/Modules/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from '../../Components/app/app.component';
-
 import * as Config from '../../../../config.json';
+
+import { AppComponent } from 'src/app/Components/app/app.component';
+import { FooterComponent } from 'src/app/Components/footer/footer.component';
+import { HeaderComponent } from 'src/app/Components/header/header.component';
+import { FilmsListComponent } from 'src/app/Components/filmslist/filmslist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    FilmsListComponent
   ],
   imports: [
     BrowserModule
