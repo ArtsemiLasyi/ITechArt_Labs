c18747a9dd43375d8963a149a2161ddccd1223ec
ArtsemiLasyi <lasyartsemy01@gmail.com>
2021-04-22 13:26:21 +0300

    Added tables "Users" and "UserRoles" with all columns and constraints

---
+++ Backend/Database/dbo/Tables/Users.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT            NOT NULL,
+    [RoleId]   TINYINT        NOT NULL,
+    [Email]    NVARCHAR (100) NOT NULL,
+    [Password] NVARCHAR (50)  NOT NULL,
+    [Salt]     NVARCHAR (20)  NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Users_UserRoles] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[UserRoles] ([Id]),
+    CONSTRAINT [UQ_Email] UNIQUE NONCLUSTERED ([Email] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Describes the user who performs actions in the application', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Users';
+
