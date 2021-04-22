f45102c1c3f6b4b6959eb69ad2a5ab9d07dae660
ArtsemiLasyi <lasyartsemy01@gmail.com>
2021-04-22 16:27:18 +0300

    Changed code style. Fixed collation to "Latin". Renamed indexes. Corrected "Email" column size

--- Backend/Database/dbo/Tables/UserRoles.sql
+++ Backend/Database/dbo/Tables/UserRoles.sql
@@ -1,11 +1,19 @@
 ﻿CREATE TABLE [dbo].[UserRoles] (
-    [Id]   TINYINT       NOT NULL,
+    [Id] TINYINT NOT NULL,
     [Name] NVARCHAR (50) NOT NULL,
     CONSTRAINT [PK_UserRoles] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [UQ_Name] UNIQUE NONCLUSTERED ([Name] ASC)
+    CONSTRAINT [UIX_UserRoles_Name] UNIQUE NONCLUSTERED ([Name] ASC)
 );
 
 
+
+
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Describes the role that the user has (administrator, common user, etc.)', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'UserRoles';
+EXECUTE sp_addextendedproperty 
+    @name = N'MS_Description',
+    @value = N'Describes the role that the user has (administrator, common user, etc.)',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'UserRoles';
 
