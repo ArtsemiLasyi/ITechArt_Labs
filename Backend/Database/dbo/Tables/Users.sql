38f1ab325b56620bf362d1223b68bdd7912456d5
ArtsemiLasyi <lasyartsemy01@gmail.com>
2021-04-23 10:56:41 +0300

    Changed types of RoleId, PasswordHash. Changed size of email. Deleted unnecessary information. Added autoincrement

--- Backend/Database/dbo/Tables/Users.sql
+++ Backend/Database/dbo/Tables/Users.sql
@@ -1,23 +1,10 @@
 ﻿CREATE TABLE [dbo].[Users] (
-    [Id] INT NOT NULL,
-    [RoleId] TINYINT NOT NULL,
-    [Email] NVARCHAR (254) NOT NULL,
-    [Password] NVARCHAR (50) NOT NULL,
+    [Id] INT IDENTITY (1, 1) NOT NULL,
+    [RoleId] INT NOT NULL,
+    [Email] NVARCHAR (320) NOT NULL,
+    [PasswordHash] BINARY (32) NOT NULL,
     [Salt] NVARCHAR (20) NOT NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Users_UserRoles] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[UserRoles] ([Id]),
     CONSTRAINT [UIX_Users_Email] UNIQUE NONCLUSTERED ([Email] ASC)
 );
-
-
-
-
-GO
-EXECUTE sp_addextendedproperty 
-    @name = N'MS_Description',
-    @value = N'Describes the user who performs actions in the application',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'Users';
-
