96c2c88a75178e4376e4506c8462be82915d1123
ArtsemiLasyi <lasyartsemy01@gmail.com>
2021-06-09 19:24:01 +0300

    Added new table "films"

---
+++ Backend/Database/dbo/Tables/Films.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Films]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [PhotoPath] NVARCHAR(260) NULL, 
+    [Year] INT NOT NULL, 
+    [Duration] INT NOT NULL, 
+    CONSTRAINT [CK_Film_Year] CHECK (Year >= 0), 
+    CONSTRAINT [CK_Films_Duration] CHECK (Duration > 0)
+)
