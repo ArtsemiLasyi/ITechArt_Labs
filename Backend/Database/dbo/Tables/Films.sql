bf4bb7e93d69999b618a3b6fcca4c8ba8fa27dd6
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2021-06-10 10:40:59 +0300

    Added new table "films" (#27)
    * Added new table "films"
    
    * Fixed "films" table

---
+++ Backend/Database/dbo/Tables/Films.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Films]
+(
+    [Id] INT IDENTITY(1,1) NOT NULL, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [PosterFileName] NVARCHAR(200) NOT NULL, 
+    [ReleaseYear] INT NOT NULL, 
+    [DurationInTicks] BIGINT NOT NULL,
+    CONSTRAINT [PK_Films] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [CK_Film_Year] CHECK (ReleaseYear >= 0), 
+    CONSTRAINT [CK_Films_Duration] CHECK (DurationInTicks > 0)
+)
